c690d7e894f554b7fa001a5fb0ee64edb21ecd59
Karthika Krishnankutty <karthikamannathu@gmail.com>
2026-01-30 23:51:32 +0530

    now display the note cards

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addNotes = document.querySelector('#add-notes-btn');
 const noNotesList = document.querySelector('.no-notes');
 const noNotes = document.querySelector('.placeholder');
 const writeNote = document.querySelector('.notes-write-pannel');
+const noteCards = document.querySelector('#note-cards');
 
 addNotes.addEventListener('click',addNotesfun)
 
@@ -13,5 +14,6 @@ writeNote.innerHTML =`<ul class = "write-note">
 <input id ="note-write-tittle" placeholder=" Note Title"> 
 <input id="note-write-desc" placeholder ="Start typing..">
 </ul>`
+noteCards.style.display = 'block'
 }
 
