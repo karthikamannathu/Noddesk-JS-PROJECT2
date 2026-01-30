2badf2568f176c5fcddc2780bb2527f78f5c76cb
Karthika Krishnankutty <karthikamannathu@gmail.com>
2026-01-30 23:42:17 +0530

    add inner HTML ul write inputes

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const addNotes = document.querySelector('#add-notes-btn');
+const noNotesList = document.querySelector('.no-notes');
+const noNotes = document.querySelector('.placeholder');
+const writeNote = document.querySelector('.notes-write-pannel');
 
-console.log(addNotes)
+addNotes.addEventListener('click',addNotesfun)
+
+
+function addNotesfun(){
+noNotesList.style.display = 'none';
+noNotes.style.display = 'none';
+writeNote.innerHTML =`<ul class = "write-note">
+<input id ="note-write-tittle" placeholder=" Note Title"> 
+<input id="note-write-desc" placeholder ="Start typing..">
+</ul>`
+}
+
